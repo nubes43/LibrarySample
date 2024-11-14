d1dc2659cafad60185d7826a38b4209b4c740449
nubes43 <93606391+nubes43@users.noreply.github.com>
2024-11-14 14:09:28 +0100

    Create library.py

---
+++ library.py
@@ -0,0 +1,32 @@
+from book import Book
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author):
+        book = Book(title, author)
+        self.books.append(book)
+        print(f'Book "{title}" by {author} added to the library.')
+
+    def borrow_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                if book.borrow():
+                    print(f'You have borrowed "{title}".')
+                    return
+                else:
+                    print(f'Sorry, "{title}" is currently borrowed.')
+                    return
+        print(f'Sorry, "{title}" is not available in the library.')
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                if book.return_book():
+                    print(f'You have returned "{title}".')
+                    return
+                else:
+                    print(f'Error: "{title}" was not borrowed.')
+                    return
+        print(f'Sorry, "{title}" is not recognized by the library.')
